6cf794962ab7913fdf70d0d74784f216ef36fd57
1498047434 <49399610+1498047434@users.noreply.github.com>
2019-05-11 00:08:11 +0800

    我的

---
+++ YanWeimin/Graph/AdjacencyMatrixHelper.h
@@ -0,0 +1,15 @@
+//
+// Created by dingmengdi on 2019/4/29.
+//
+
+#ifndef GRAPH_ADJACENCYMATRIXHELPER_H
+#define GRAPH_ADJACENCYMATRIXHELPER_H
+
+#define MAXInt 32767
+#define MVNum 100
+
+typedef struct {
+
+};
+
+#endif //GRAPH_ADJACENCYMATRIXHELPER_H
