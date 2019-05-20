51c7bc20438055b85190a36640a6d468ef3cc8f5
1498047434 <49399610+1498047434@users.noreply.github.com>
2019-05-20 14:40:00 +0800

    最大堆完成， 索引堆完成。

---
+++ MaxHeap/InsertionSort.h
@@ -0,0 +1,57 @@
+//
+// Created by Dingmengdi on 2019/4/24.
+//
+
+#ifndef BUBBLINGSORT_INSERTIONSORT_H
+#define BUBBLINGSORT_INSERTIONSORT_H
+
+
+template <typename T>
+void insertionSort(T arr[], int n){
+    for (int i = 1; i < n; ++i) {
+        //寻找元素arr[i]合适的插入位置
+        for(int j=i; j>0; j--){
+            if(arr[j] < arr[j-1]){
+                swap(arr[j], arr[j-1]);
+            }else{
+                break;
+            }
+        }
+    }
+}
+
+template <typename T>
+void insertionSort2(T arr[], int n){
+    for (int i = 1; i < n; ++i) {
+        //寻找元素arr[i]合适的插入位置
+        T e = arr[i];
+        int j;
+        for(j=i; j>0; j--){
+            if(e < arr[j-1]){
+                arr[j] = arr[j-1];
+            }else{
+                break;
+            }
+        }
+        arr[j] = e;
+    }
+}
+
+template <typename T>
+void insertionSort2(T arr[], int l, int r){
+    for (int i = l+1; i < r+1 ; ++i) {
+        //寻找元素arr[i]合适的插入位置
+        T e = arr[i];
+        int j;
+        for(j=i; j>0; j--){
+            if(e < arr[j-1]){
+                arr[j] = arr[j-1];
+            }else{
+                break;
+            }
+        }
+        arr[j] = e;
+    }
+}
+
+#endif //BUBBLINGSORT_INSERTIONSORT_H
--- MaxHeap/main.cpp
+++ MaxHeap/main.cpp
@@ -1,20 +1,94 @@
+#include "Heap.h"
+#include "MergeSort.h"
+#include "QuickSort.h"
+#include "SortTestHelper.h"
+#include "HeapSort.h"
 
 
 
 int main() {
 
-    MaxHeap<int> maxHeap = MaxHeap<int>(100);
-    cout<<maxHeap.size()<<endl;
+    int n = 100000;
 
-//    srand(time(NULL));
-    for (int i = 0; i < 50; ++i) {
-        maxHeap.insert( rand()%100);
-    }
-//    maxHeap.testPrint();
+    //测试一  一般性测试
+    cout<<"1  Test for Random Array, size = "<<n<<", random rang [0, "<<n<<"]"<<endl;
+    int *arr1 = SortTestHelper::generateRandomArray(n,0,n);
+    int *arr2 = SortTestHelper::copyIntArray(arr1,n);
+    int *arr3 = SortTestHelper::copyIntArray(arr1,n);
+    int *arr4 = SortTestHelper::copyIntArray(arr1,n);
+    int *arr5 = SortTestHelper::copyIntArray(arr1,n);
+    int *arr6 = SortTestHelper::copyIntArray(arr1,n);
+    int *arr7 = SortTestHelper::copyIntArray(arr1,n);
 
-    while (!maxHeap.isEmpty()){
-        cout<<maxHeap.extracMax()<<" ";
-    }
+    SortTestHelper::testSort("mergeSort", mergeSort, arr1,n);
+    SortTestHelper::testSort("quickSort", quickSort, arr2,n);
+    SortTestHelper::testSort("quickSort3Ways", quickSort3Ways, arr3,n);
+    SortTestHelper::testSort("heapSort1", heapSort1, arr4,n);
+    SortTestHelper::testSort("heapSort2", heapSort2, arr5,n);
+    SortTestHelper::testSort("heapSort", heapSort, arr6,n);
+    SortTestHelper::testSort("IndexheapSort", IndexheapSort, arr7,n);
 
-    return 0;
+    delete[] arr1;
+    delete[] arr2;
+    delete[] arr3;
+    delete[] arr4;
+    delete[] arr5;
+    delete[] arr6;
+    delete[] arr7;
+    cout<<endl;
+
+
+    //2 测试近乎有序的数组
+    int swapTimes = 100;
+    cout<<"2   Test for Random Nearly Ordered Array, size = "<<n<<", random rang [0, "<<n<<"]"<<endl;
+    arr1 = SortTestHelper::generateNearlyOrderArray(n,swapTimes);
+    arr2 = SortTestHelper::copyIntArray(arr1,n);
+    arr3 = SortTestHelper::copyIntArray(arr1,n);
+    arr4 = SortTestHelper::copyIntArray(arr1,n);
+    arr5 = SortTestHelper::copyIntArray(arr1,n);
+    arr6 = SortTestHelper::copyIntArray(arr1,n);
+
+    SortTestHelper::testSort("mergeSort", mergeSort, arr1,n);
+    SortTestHelper::testSort("quickSort", quickSort, arr2,n);
+    SortTestHelper::testSort("quickSort3Ways", quickSort3Ways, arr3,n);
+    SortTestHelper::testSort("heapSort1", heapSort1, arr4,n);
+    SortTestHelper::testSort("heapSort2", heapSort2, arr5,n);
+    SortTestHelper::testSort("heapSort", heapSort, arr6,n);
+
+    delete[] arr1;
+    delete[] arr2;
+    delete[] arr3;
+    delete[] arr4;
+    delete[] arr5;
+    delete[] arr6;
+    cout<<endl;
+
+
+
+    //测试3 测试含有存在大量相同元素的数组
+    cout<<"3   Test for Random Array and almost the same size = "<<n<<", random rang [0, "<<10<<"]"<<endl;
+    arr1 = SortTestHelper::generateRandomArray(n,0,10);
+    arr2 = SortTestHelper::copyIntArray(arr1,n);
+    arr3 = SortTestHelper::copyIntArray(arr1,n);
+    arr4 = SortTestHelper::copyIntArray(arr1,n);
+    arr5 = SortTestHelper::copyIntArray(arr1,n);
+    arr6 = SortTestHelper::copyIntArray(arr1,n);
+
+    SortTestHelper::testSort("mergeSort", mergeSort, arr1,n);
+    SortTestHelper::testSort("quickSort", quickSort, arr2,n);
+    SortTestHelper::testSort("quickSort3Ways", quickSort3Ways, arr3,n);
+    SortTestHelper::testSort("heapSort1", heapSort1, arr4,n);
+    SortTestHelper::testSort("heapSort2", heapSort2, arr5,n);
+    SortTestHelper::testSort("heapSort", heapSort, arr6,n);
+
+    delete[] arr1;
+    delete[] arr2;
+    delete[] arr3;
+    delete[] arr4;
+    delete[] arr5;
+    delete[] arr6;
+    cout<<endl;
+
+
+
 }
